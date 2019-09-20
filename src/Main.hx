78927e1d498557987d035d8b9fd07cfc0d315fe6
Joey <joeyprather8@gmail.com>
2019-09-20 02:55:01 -0700

    add notes

--- src/Main.hx
+++ src/Main.hx
@@ -1,6 +1,7 @@
 //file
 import sys.FileSystem;
 import sys.io.File;
+import sys.FileStat;
 //path
 import haxe.io.Path;
 using StringTools;
@@ -10,6 +11,12 @@ import haxe.xml.Parser;
 class Main {
 	static function main() 
 	{
+		/**
+		 * Notes
+		 * FileSystem.stat(path);
+		 * FileSystem.rename(path,newpath);
+		 * File.copy(path,newpath);
+		 */
 		//simple scripting environment in order to maniputlate data if need be.
 		trace("start script");
 		//script
