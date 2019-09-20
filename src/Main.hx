e9f53b47e3e325fd7d333d59102dce477527e7e5
Joey <joeyprather8@gmail.com>
2019-09-20 02:13:54 -0700

    prep haxe

---
+++ src/Main.hx
@@ -0,0 +1,17 @@
+//file
+import sys.FileSystem;
+import sys.io.File;
+//path
+import haxe.io.Path;
+using StringTools;
+//html
+import haxe.xml.Fast;
+import haxe.xml.Parser;
+class Main {
+	static function main() 
+	{
+		//simple scripting environment in order to maniputlate data if need be.
+		trace("start script");
+		//script
+	}
+}
